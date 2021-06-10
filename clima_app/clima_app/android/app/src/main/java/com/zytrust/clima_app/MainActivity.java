81d02c8285342d4a2e55b9c87928ac890eba801a
jcabelloc <jcabelloc@itana.pe>
2021-06-09 22:52:53 -0500

    clima - 1era parte - agregar location_screen

---
+++ clima_app/clima_app/android/app/src/main/java/com/zytrust/clima_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.zytrust.clima_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ clima_app/clima_app/lib/screens/location_screen.dart
@@ -0,0 +1,88 @@
+import 'package:clima_app/utilities/constants.dart';
+import 'package:flutter/material.dart';
+
+class LocationScreen extends StatefulWidget {
+  LocationScreen();
+
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  int temperature = 18;
+  String weatherIcon = '☁️';
+  String cityName = 'Lima';
+  String weatherMessage = 'Todo esta muy cool';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/location_background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.dstATop),
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () async {},
+                    icon: Icon(
+                      Icons.near_me,
+                      size: 50.0,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () async {},
+                    icon: Icon(
+                      Icons.location_city,
+                      size: 50.0,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '$temperature°',
+                      style: kTempTextStyle,
+                    ),
+                    Text(
+                      weatherIcon,
+                      style: kConditionTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: Text(
+                  '$weatherMessage in $cityName!',
+                  textAlign: TextAlign.right,
+                  style: kMessageTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
